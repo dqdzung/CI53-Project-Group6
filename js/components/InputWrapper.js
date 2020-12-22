702ed27642f5dd7c3c2152c8d0b2bd319690c29a
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-12-22 08:13:15 +0700

    input-wrapper

---
+++ js/components/InputWrapper.js
@@ -0,0 +1,63 @@
+const $template = document.createElement("template");
+
+$template.innerHTML = /*html*/ `
+ <div id="input-wrapper">
+    <label id="input-label" for="#input-main"></label>
+    <input id="input-main" type="text">
+    <div id="input-error"></div>
+ </div>
+`;
+
+export default class InputWrapper extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild($template.content.cloneNode(true));
+
+    this.$label = this.shadowRoot.getElementById("input-label");
+    this.$main = this.shadowRoot.getElementById("input-main");
+    this.$error = this.shadowRoot.getElementById("input-error");
+  }
+
+  static get observedAttributes() {
+    return ["label", "type", "error", "value"];
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    switch (attrName) {
+      case "label":
+        this.$label.innerHTML = newValue;
+        break;
+      case "type":
+        this.$main.type = newValue;
+        break;
+      case "error":
+        this.$error.innerHTML = newValue;
+        break;
+      case "value":
+        this.$main.value = newValue;
+        break;
+    }
+  }
+
+  value(){
+      return this.$main.value;
+  }
+
+  error(message) {
+    this.setAttribute("error", message);
+  }
+
+  static validate($inputWrapper, condition, message) {
+    let value = $inputWrapper.value();
+    if (condition(value)) {
+      $inputWrapper.error("");
+      return true;
+    } else {
+      $inputWrapper.error(message);
+      return false;
+    }
+  }
+}
+
+window.customElements.define("input-wrapper", InputWrapper);
